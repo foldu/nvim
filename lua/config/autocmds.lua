d9c9f33e19d08558e786839dd4f5fd99fb977270
foldu <foldu@protonmail.com>
2025-06-07 18:34:35 +0200

    Copy to clipboard on window focus lost

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,8 +2,8 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- Restore cursor to file position in previous editing session
 vim.api.nvim_create_autocmd("BufReadPost", {
+    desc = "Restore cursor to file position in previous editing session",
     callback = function(args)
         local mark = vim.api.nvim_buf_get_mark(args.buf, '"')
         local line_count = vim.api.nvim_buf_line_count(args.buf)
@@ -13,7 +13,15 @@ vim.api.nvim_create_autocmd("BufReadPost", {
     end,
 })
 
--- Auto resize splits when the terminal's window is resized
 vim.api.nvim_create_autocmd("VimResized", {
+    desc = "Auto resize splits when the terminal's window is resized",
     command = "wincmd =",
+})
+
+-- https://old.reddit.com/r/neovim/comments/1l4tubm/copy_last_yanked_text_to_clipboard_on_focuslost/
+vim.api.nvim_create_autocmd("FocusLost", {
+    desc = "Copy to clipboard on FocusLost",
+    callback = function()
+        vim.fn.setreg("+", vim.fn.getreg("0"))
+    end,
 })
