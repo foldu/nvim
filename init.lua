2435057bed603aa83d9e00d07711ead8ba23ad62
foldu <foldu@protonmail.com>
2026-06-13 07:12:17 +0200

    Fix config exploding on windows

--- init.lua
+++ init.lua
@@ -15,6 +15,6 @@ require("plugins")
 -- if this kind of looks like a dev machine use lsp
 -- root should preferably not download random shit from the internet
 -- windows IO is too slow to be usable, so don't create an lsp setup on it
-if vim.loop.getuid() ~= 0 and not (vim.fn.has("win32") == 1) and vim.fn.executable("rustup") then
+if vim.fn.has("win32") ~= 1 and vim.loop.getuid() ~= 0 and vim.fn.executable("rustup") then
     require("lsp")
 end
